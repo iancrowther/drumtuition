8f1c078cbcadd9240e46f49d7a661ca25c5a3034
unknown <Ian Crowther@.(none)>
2009-12-08 14:08:09 +0000

    first upload of drumtuition src

---
+++ engine/js/_vti_cnf/videogallery.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|30 Nov 2009 21:06:55 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|LAPTOP\\Ian Crowther
+vti_modifiedby:SR|LAPTOP\\Ian Crowther
+vti_timecreated:TR|28 Nov 2009 15:00:20 -0000
+vti_backlinkinfo:VX|contact.html index.html
+vti_nexttolasttimemodified:TW|29 Nov 2009 15:53:52 -0000
+vti_cacheddtm:TX|30 Nov 2009 21:06:55 -0000
+vti_filesize:IR|2113
