f531594422ed039ec6eb315cb0be7552759fd87d
unknown <Ian Crowther@.(none)>
2009-12-09 13:11:49 +0000

    modified sitemap and meta data

--- engine/js/_vti_cnf/swfobject.js
+++ engine/js/_vti_cnf/swfobject.js
@@ -1,9 +1,12 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|03 Nov 2009 10:04:29 -0000
-vti_extenderversion:SR|12.0.0.0
+vti_timelastmodified:TX|03 Nov 2009 10:04:29 -0000
 vti_author:SR|LAPTOP\\Ian Crowther
 vti_modifiedby:SR|LAPTOP\\Ian Crowther
-vti_timecreated:TR|03 Nov 2009 10:04:29 -0000
-vti_cacheddtm:TX|03 Nov 2009 10:04:29 -0000
+vti_nexttolasttimemodified:TX|03 Nov 2009 10:04:29 -0000
+vti_timecreated:TR|08 Dec 2009 22:08:16 -0000
+vti_cacheddtm:TX|08 Dec 2009 22:08:16 -0000
 vti_filesize:IR|9759
+vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|contact.html index.html
+vti_syncwith_localhost\\c\:\\users\\ian crowther\\drumtuition/c\:/users/ian crowther/drumtuition:TW|08 Dec 2009 22:08:16 -0000
+vti_syncofs_localhost\\c\:\\users\\ian crowther\\drumtuition/c\:/users/ian crowther/drumtuition:TX|03 Nov 2009 10:04:29 -0000
