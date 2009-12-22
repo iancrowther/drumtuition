191053f6d36b53055d681e6cd81a56d3a1019355
unknown <Ian Crowther@.(none)>
2009-12-22 21:03:59 +0000

    Updated UI and keywords

--- engine/js/_vti_cnf/videogallery.js
+++ engine/js/_vti_cnf/videogallery.js
@@ -7,6 +7,6 @@ vti_timecreated:TR|08 Dec 2009 22:08:16 -0000
 vti_cacheddtm:TX|08 Dec 2009 22:08:16 -0000
 vti_filesize:IR|2113
 vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|contact.html index.html
+vti_backlinkinfo:VX|contact.html thankyou.html index.html
 vti_syncwith_localhost\\c\:\\users\\ian crowther\\drumtuition/c\:/users/ian crowther/drumtuition:TW|08 Dec 2009 22:08:16 -0000
 vti_syncofs_localhost\\c\:\\users\\ian crowther\\drumtuition/c\:/users/ian crowther/drumtuition:TX|30 Nov 2009 21:06:55 -0000
